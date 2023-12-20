4d4063c0ee2899546315cc545550515bf834ed21
Piotr Samczuk <samczukp@gmail.com>
2023-12-20 17:34:59 +0100

    add calibration support for both cameras

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,37 @@
 #include <utility>
 #include <string>
 #include <memory>
+#include <cstdint>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
 #define LEFTCAMINDEX 0
 #define RIGHTCAMINDEX 1
 #define BOARDWIDTH 11
 #define BOARDHEIGHT 8
-#define CHECKERSIZE 12 // [mm]
-#define SQUARESIZE ((CHECKERSIZE * CHECKERSIZE) * 0.01) // [sqmm]
-#define NUMBEROFIMAGES 50
+#define CHECKERSIZE 17 // [mm]
+#define SQUARESIZE (CHECKERSIZE * CHECKERSIZE) // [sqmm]
+#define NUMBEROFIMAGES 20
+
+class OpenSami
+{
+public:
+    OpenSami(const uint8_t leftCameraIndex, const uint8_t rightCameraIndex);
+    void initialize();
+private:
+    const uint8_t leftCameraIndex = LEFTCAMINDEX;
+    const uint8_t rightCameraIndex = RIGHTCAMINDEX;
+    cv::VideoCapture capLeft;
+    cv::VideoCapture capRight;
+};
+
+OpenSami::OpenSami(const uint8_t leftCameraIndex, const uint8_t rightCameraIndex)
+    : leftCameraIndex(leftCameraIndex)
+    , rightCameraIndex(rightCameraIndex)
+    {}
 
 
 void getCameraIndexes()
@@ -109,45 +130,81 @@ void drawAxis(cv::Mat &_image, cv::InputArray _cameraMatrix, cv::InputArray _dis
 void writeCalibrationData(const cv::Mat& K, const cv::Mat& D)
 {
     //Writing the data in a YAML file
-    std::string out_file = "calibration.txt";
-    cv::FileStorage fs(out_file, cv::FileStorage::WRITE);
+    std::string outputFile = "calibration.txt";
+    cv::FileStorage fs(outputFile, cv::FileStorage::WRITE);
+    if(not fs.isOpened())
+    {
+        std::cerr << "Error saving calibration data." << std::endl;
+        return;
+    }
     fs << "K" << K;
     fs << "D" << D;
     fs << "board_width" << BOARDWIDTH;
     fs << "board_height" << BOARDHEIGHT;
-    fs << "square_size" << SQUARESIZE;
-    printf("Done Calibration\n");
+    fs << "square_size" << CHECKERSIZE;
+    printf("Done writing calibration data\n");
+}
+
+void readCalibrationData()
+{
+    cv::Mat K, D;
+    std::string inputFile = "calibration.txt";
+    cv::FileStorage fs(inputFile, cv::FileStorage::READ);
+    if(not fs.isOpened())
+    {
+        std::cerr << "Error reading calibration data." << std::endl;
+        return;
+    }
+
+
+    printf("Done reading calibration data\n");
 }
 
 void captureImagesForCalibration(cv::VideoCapture& capLeft, cv::VideoCapture& capRight, cv::Size boardSize,
-    std::vector<std::vector<cv::Point2f>>& imagePoints, std::vector<std::vector<cv::Point3f>>& objectPoints, cv::Mat& matImg)
+    std::vector<std::vector<cv::Point2f>>& imagePointsL, std::vector<std::vector<cv::Point2f>>& imagePointsR, std::vector<std::vector<cv::Point3f>>& objectPoints, cv::Mat& matImgL, cv::Mat& matImgR)
 {
     // Loop through video stream until we capture enough images (num_imgs) of checkerboard
     while(true)
     {
-        capRight >> matImg;
-        if(matImg.empty())
+        capLeft >> matImgL;
+        capRight >> matImgR;
+        if(matImgL.empty() or matImgR.empty())
         {
             break;
         }
         // Сheckerboard corner coordinates in the image
-        std::vector<cv::Point2f> corners;
+        std::vector<cv::Point2f> cornersL;
+        std::vector<cv::Point2f> cornersR;
         // Here we find all the corner points of each image and their corresponding 3D world points
         // and prepare the corresponding vectors.
         // Find all the checkerboard corners
-        bool found = cv::findChessboardCorners(matImg, boardSize, corners, cv::CALIB_CB_FAST_CHECK);
-        if(found)
+        bool foundL = cv::findChessboardCorners(matImgL, boardSize, cornersL, cv::CALIB_CB_ADAPTIVE_THRESH);
+        bool foundR = cv::findChessboardCorners(matImgR, boardSize, cornersR, cv::CALIB_CB_ADAPTIVE_THRESH);
+        cv::Size winSize = cv::Size( 12, 12 );
+        cv::Size zeroZone = cv::Size( -1, -1 );
+        cv::TermCriteria criteria = cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1 );
+        if(foundL)
         {
-            cv::Size winSize = cv::Size( 12, 12 );
-            cv::Size zeroZone =cv::Size( -1, -1 );
-            cv::TermCriteria criteria = cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1 );
             // Convert image to grayscale images
-            cv::Mat src_gray;
-            cvtColor( matImg, src_gray, cv::COLOR_BGR2GRAY );
+            cv::Mat grayL;
+            cvtColor(matImgL, grayL, cv::COLOR_BGR2GRAY);
             // Find more exact corner positions (more exact than integer pixels)
-            cv::cornerSubPix( src_gray, corners, winSize, zeroZone, criteria );
+            cv::cornerSubPix( grayL, cornersL, winSize, zeroZone, criteria );
             // This function helps to visualize the checkerboard corners found (optional)
-            cv::drawChessboardCorners(matImg, boardSize, cv::Mat(corners), found);
+            cv::drawChessboardCorners(matImgL, boardSize, cv::Mat(cornersL), foundL);
+        }
+        if(foundR)
+        {
+            // Convert image to grayscale images
+            cv::Mat grayR;
+            cvtColor(matImgR, grayR, cv::COLOR_BGR2GRAY);
+            // Find more exact corner positions (more exact than integer pixels)
+            cv::cornerSubPix( grayR, cornersR, winSize, zeroZone, criteria );
+            // This function helps to visualize the checkerboard corners found (optional)
+            cv::drawChessboardCorners(matImgR, boardSize, cv::Mat(cornersR), foundR);
+        }
+        if(foundL and foundR)
+        {
             // Prepare the objectPoints and imagePoints vectors
             std::vector<cv::Point3f> obj;
             for (int i = 0; i < BOARDHEIGHT - 1; i++)
@@ -158,13 +215,15 @@ void captureImagesForCalibration(cv::VideoCapture& capLeft, cv::VideoCapture& ca
                 }
             }
             std::cout <<"Found corners!" << std::endl;
-            imagePoints.push_back(corners);
+            imagePointsL.push_back(cornersL);
+            imagePointsR.push_back(cornersR);
             objectPoints.push_back(obj);
         }
-        cv::imshow("Camera", matImg);
-        cv::waitKey(1);
+        cv::imshow("Camera left", matImgL);
+        cv::imshow("Camera right", matImgR);
+        cv::waitKey(100);
         // Esc to exit if we have enough images of checkerboard
-        if (imagePoints.size() == NUMBEROFIMAGES)
+        if (imagePointsL.size() == NUMBEROFIMAGES and imagePointsR.size() == NUMBEROFIMAGES)
         {
             break;
         }
@@ -174,38 +233,51 @@ void captureImagesForCalibration(cv::VideoCapture& capLeft, cv::VideoCapture& ca
 void calibrate(cv::VideoCapture& capLeft, cv::VideoCapture& capRight)
 {
     //Checkerboard corner coordinates in the image
-    std::vector<std::vector<cv::Point2f>> imagePoints;
     //Object points are the actual 3D coordinate of checkerboard points
-    std::vector<std::vector<cv::Point3f>> objectPoints;
     // Define size of calibration board 
+    std::vector<std::vector<cv::Point2f>> imagePointsL, imagePointsR;
+    std::vector<std::vector<cv::Point3f>> objectPoints;
     cv::Size boardSize(BOARDWIDTH - 1, BOARDHEIGHT - 1);
-    cv::Mat matImg;
-    captureImagesForCalibration(capLeft, capRight, boardSize, imagePoints, objectPoints, matImg);
+    cv::Mat matImgL, matImgR;
+    captureImagesForCalibration(capLeft, capRight, boardSize, imagePointsL, imagePointsR, objectPoints, matImgL, matImgR);
     printf("Starting Calibration\n");
     //K contains the intrinsics
-    cv::Mat K;
     //D contains the distortion coefficients
-    cv::Mat D;
     //The rotation and translation vectors
-    std::vector<cv::Mat> rvecs, tvecs;
-    //Set flag to ignore higher order distortion coefficients k4 and k5.
+    cv::Mat KL, DL;
+    cv::Mat KR, DR;
+    std::vector<cv::Mat> rvecsL, tvecsL;
+    std::vector<cv::Mat> rvecsR, tvecsR;
+    //The rotation and translation vectors for cameras in stereo
+    std::vector<cv::Mat> rvecsS, tvecsS;
+    // Essential Matrix and fundamental matrix
+    cv::Mat ES, FS;
+    //Set flag
     int flag = cv::CALIB_FIX_K3 + cv::CALIB_ZERO_TANGENT_DIST + cv::CALIB_FIX_PRINCIPAL_POINT + cv::CALIB_FIX_ASPECT_RATIO;
-    std::cout << matImg.size() << std::endl;
-    float reprojectionError = cv::calibrateCamera(objectPoints, imagePoints, matImg.size(), K, D, rvecs, tvecs, flag);
-    std::cout << "DUPALOG" << std::endl;
+    double reprojectionErrorL = cv::calibrateCamera(objectPoints, imagePointsL, matImgL.size(), KL, DL, rvecsL, tvecsL, flag);
+    double reprojectionErrorR = cv::calibrateCamera(objectPoints, imagePointsR, matImgR.size(), KR, DR, rvecsR, tvecsR, flag);
+    //ponizej nie dziala
+    double reprojectionErrorLR = cv::stereoCalibrate(objectPoints, imagePointsL, imagePointsR, matImgL, DL, matImgR, DR, matImgL.size(), rvecsS, tvecsS, ES, FS, cv::CALIB_FIX_INTRINSIC);
+    std::cout << "reprojectionErrorL: " << reprojectionErrorL << std::endl;
+    std::cout << "reprojectionErrorR: " << reprojectionErrorR << std::endl;
+    std::cout << "reprojectionErrorLR: " << reprojectionErrorLR << std::endl;
     while(true)
     {
-        capLeft >> matImg;
-        if(matImg.empty())
+        capLeft >> matImgL;
+        capRight >> matImgR;
+        if(matImgL.empty() or matImgR.empty())
         {
+            std::cerr << "Error reading from camera capture after calibration." << std::endl;
             break;
         }
         //Found chessboard corners
-        std::vector<cv::Point2f> imagePoints;
-        bool found = cv::findChessboardCorners(matImg, boardSize, imagePoints, cv::CALIB_CB_FAST_CHECK);
-        if(found)
+        std::vector<cv::Point2f> imagePointsL, imagePointsR;
+        bool foundL = cv::findChessboardCorners(matImgL, boardSize, imagePointsL, cv::CALIB_CB_ADAPTIVE_THRESH);
+        bool foundR = cv::findChessboardCorners(matImgR, boardSize, imagePointsR, cv::CALIB_CB_ADAPTIVE_THRESH);
+        if(foundL or foundR)
         {
-            cv::drawChessboardCorners(matImg, boardSize, cv::Mat(imagePoints), found);
+            cv::drawChessboardCorners(matImgL, boardSize, cv::Mat(imagePointsL), foundL);
+            cv::drawChessboardCorners(matImgR, boardSize, cv::Mat(imagePointsR), foundR);
 
             std::vector<cv::Point3f> obj;
             for (int i = 0; i < BOARDHEIGHT - 1; i++)
@@ -216,14 +288,19 @@ void calibrate(cv::VideoCapture& capLeft, cv::VideoCapture& capRight)
                 }
             }
             //SolvePnP
-            cv::Mat rvec, tvec;
-            cv::solvePnP(obj, imagePoints, K, D, rvec, tvec);
+            cv::Mat rvecL, tvecL;
+            cv::Mat rvecR, tvecR;
+            cv::solvePnP(obj, imagePointsL, KL, DL, rvecL, tvecL);
+            cv::solvePnP(obj, imagePointsR, KR, DR, rvecR, tvecR);
 
-            drawAxis(matImg, K, D, rvec, tvec, CHECKERSIZE);
-            std::cout << "Distance to chessboard: "  << cv::norm(tvec) << std::endl;
+            drawAxis(matImgL, KL, DL, rvecL, tvecL, CHECKERSIZE);
+            drawAxis(matImgR, KR, DR, rvecR, tvecR, CHECKERSIZE);
+            std::cout << "Distance to chessboard from left camera: "  << cv::norm(tvecL) << std::endl;
+            std::cout << "Distance to chessboard from right camera: "  << cv::norm(tvecR) << std::endl;
         }
 
-        cv::imshow("Camera", matImg);
+        cv::imshow("Camera left", matImgL);
+        cv::imshow("Camera right", matImgR);
         auto c = cv::waitKey(30);
 
         // 27 == ESC
@@ -232,7 +309,7 @@ void calibrate(cv::VideoCapture& capLeft, cv::VideoCapture& capRight)
             break;
         }
     }
-    writeCalibrationData(K, D);
+    writeCalibrationData(KL, DL);
 }
 
 int main()
@@ -240,8 +317,9 @@ int main()
     // Start video capture of both cameras
     auto [capLeft, capRight] = startVideoCaptures(LEFTCAMINDEX, RIGHTCAMINDEX);
 
-    //viewCombinedCameraFeeds(capLeft, capRight);
+    // viewCombinedCameraFeeds(capLeft, capRight);
     calibrate(capLeft, capRight);
+
     // Release the VideoCapture and close the window
     capLeft.release();
     capRight.release();
